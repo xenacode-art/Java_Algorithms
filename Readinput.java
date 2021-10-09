76239893dada53a579ad7381d78a4c76906ccbd7
xenacode-art <kaja.obinna@lmu.edu.ng>
2021-10-09 16:12:41 +0100

    code to read input from users

--- Readinput.java
+++ Readinput.java
@@ -1,10 +1,21 @@
+// package com.kaja
+
+// public  class  Main {
+//     public static void main (String[] args) {
+//         Scanner scanner  = new Scanner(system.in);
+//         system.out.println("Age: ");
+//         byte age  = scanner.nextByte();
+//         system.out.println("You are "  +  Age)
+//     }
+// }
+
 package com.kaja
 
 public  class  Main {
     public static void main (String[] args) {
         Scanner scanner  = new Scanner(system.in);
-        system.out.println("Age: ");
-        byte age  = scanner.nextByte();
-        system.out.println("You are "  +  Age)
+        system.out.println("Name: ");
+        String name  = scanner.nextLine();
+        system.out.println("You are "  +  name)
     }
 }
