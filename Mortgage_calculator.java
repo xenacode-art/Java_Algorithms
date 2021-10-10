ef4f665e97a0cd176385a1e1ac5cc326eb71a1ea
xenacode-art <kaja.obinna@lmu.edu.ng>
2021-10-10 15:01:15 +0100

    My first java Mortagage calculator

---
+++ Mortgage_calculator.java
@@ -0,0 +1,26 @@
+package com.cyberspace;
+ 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public  class  Main{
+    public static void{(String[]  args) {
+        final byte MONTH_IN_YEAR = 12;
+        final PERCENT  = 100;
+
+        Scanner scanner  = new Scanner(system.in);
+
+        system.out.println("Principal: ");
+        int principal = scanner.nextInt();
+        
+        system.out.println("Annual Intrest Rate: ");
+        float annualInterest  = scanner.nextFloat();
+        float  monthlyInterest = annualInterest / PERCENT  /MONTH_IN_YEAR;
+
+        system.out.print("Period  (Years): ");
+        byte  years = scanner.nextByte();
+        int numberOfPayments  =  years * MONTH_IN_YEAR;
+
+        double mortage  = principal  * (monthlyInterest  * Math.pow(1 + monthlyInterest, numberOfPayments ))  / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+    }
+}
