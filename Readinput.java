186e8bcfde7c528da3b1c36b7c48f179a6c99eba
xenacode-art <kaja.obinna@lmu.edu.ng>
2021-10-10 16:23:28 +0100

    update

--- Readinput.java
+++ Readinput.java
@@ -1,13 +1,13 @@
-// package com.kaja
+package com.kaja
 
-// public  class  Main {
-//     public static void main (String[] args) {
-//         Scanner scanner  = new Scanner(system.in);
-//         system.out.println("Age: ");
-//         byte age  = scanner.nextByte();
-//         system.out.println("You are "  +  Age)
-//     }
-// }
+public  class  Main {
+    public static void main (String[] args) {
+        Scanner scanner  = new Scanner(system.in);
+        system.out.println("Age: ");
+        byte age  = scanner.nextByte();
+        system.out.println("You are "  +  Age)
+    }
+}
 
 package com.kaja
 
