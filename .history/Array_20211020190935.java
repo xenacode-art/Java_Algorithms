c28c61281c3cf1600c6dd146fdbd5dbab0f73973
xenacode-art <kaja.obinna@lmu.edu.ng>
2021-10-31 18:37:42 +0100

    Added more codes to my work flow

---
+++ .history/Array_20211020190935.java
@@ -0,0 +1,21 @@
+package com.cyberspac
+
+public  class  Main {
+    public static void main (String[] args) {
+        Scanner scanner  = new Scanner(system.in);
+        system.out.println("Age: ");
+        byte age  = scanner.nextByte();
+        system.out.println("You are "  +  Age)
+    }
+}
+
+
+public  class  Main {
+    public static void main (String[] args) {
+        Scanner scanner  = new Scanner(system.in);
+        system.out.println("Name: ");
+        //use the trim method to get rid of white spaces.
+        String name  = scanner.nextLine.trim();
+        system.out.println("You are "  +  name)
+    }
+}
---
+++ .history/main_20211001181324.java
@@ -0,0 +1,13 @@
+package com.cyberspace
+
+public class Main(String[] args) {
+//run this code with the class.java file.
+    Array numbers = new Array(3);
+    numbers.insert(10);
+    numbers.insert(20);
+    numbers.insert(30);
+    numbers.insert(40);
+   // numbers.removeAt(0);
+   system.out.println(numbers.indexOf(1);
+    numbers.print(); 
+}
